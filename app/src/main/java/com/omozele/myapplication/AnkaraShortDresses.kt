97e958efa0d8980d71b24b2043655226b7f6ddb6
joytammy <lopatromo@yahoo.com>
2021-06-23 17:49:15 +0100

    initial commit3

---
+++ app/src/main/java/com/omozele/myapplication/AnkaraShortDresses.kt
@@ -0,0 +1,11 @@
+package com.omozele.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AnkaraShortDresses : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ankara_short_dresses)
+    }
+}
--- app/src/main/java/com/omozele/myapplication/Profile.kt
+++ app/src/main/java/com/omozele/myapplication/Profile.kt
@@ -2,17 +2,15 @@ package com.omozele.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.TextView
 
-class HomeActivity : AppCompatActivity() {
+class Profile : AppCompatActivity() {
 
-    private lateinit var usernameDisplay: TextView
-    private lateinit var profile: Profile
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
-
-        }
+        setContentView(R.layout.activity_profile)
 
     }
+}
